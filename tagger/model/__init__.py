a9714c0a9a55cf90697f3616d5be51343e4221c1
AKaronen <alex.karonen@gmail.com>
2025-12-01 17:49:09 +0200

    restructured models folder

--- tagger/model/__init__.py
+++ tagger/model/__init__.py
@@ -1,6 +1,9 @@
 # from tagger.model.DeepSetModel import DeepSetModel
-from tagger.model.LorentzNet import LorentzNet
-from tagger.model.DeepSetModel import DeepSetModel
-from tagger.model.ParticleTransformer import ParticleTransformer
+# Offline models
+from tagger.model.offline.LorentzNet import LorentzNet
+from tagger.model.offline.ParticleTransformer import ParticleTransformer
+
+# Online models
+from tagger.model.online.DeepSetModel import DeepSetModel
 # from tagger.model.InteractionNetModel import InteractionNetModel
 # from tagger.model.TransformerModel import TransformerModel
